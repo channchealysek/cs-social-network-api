c80e7c941581c56fa38010bb7b4d3b23f67fbe24
Channchealy Sek <sek.channchealy@yahoo.com>
2022-09-19 14:52:58 -0500

    implimeants GRUD for user api routes

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,14 +1,87 @@
+const { User, Thought } = require("../models");
+const user404Message = (id) => `User with ID: ${id} not found!`;
+const user204Message = (id) => `User with ID: ${id} has been deleted!`;
+
 const userController = {
   // get all users
-  getAllUsers(req, res) {},
+  async getAllUsers(req, res) {
+    try {
+      const dataUsers = await User.find({})
+        .populate({ path: "thoughts", select: "-__v" })
+        .populate({ path: "friends", select: "-__v" })
+        .select("-__v");
+      return res.status(200).json(dataUsers);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  },
   // get one user by ID
-  getUserById({ params }, res) {},
+  async getUserById({ params }, res) {
+    try {
+      User.findOne({ _id: params.id })
+        .populate({ path: "friends", select: "-__v" })
+        .populate({
+          path: "thoughts",
+          select: "-__v",
+          populate: { path: "reactions" },
+        })
+        .select("-__v")
+        .then((dataUser) =>
+          dataUser
+            ? res.status(200).json(dataUser)
+            : res.status(404).json({ message: user404Message(params.id) })
+        )
+        .catch((err) => res.status(404).json(err));
+    } catch (err) {
+      return res.status(400).json(err);
+    }
+  },
   // add a new user
-  createUser({ body }, res) {},
+  async createUser({ body }, res) {
+    try {
+      const dataUser = await User.create({
+        username: body.username,
+        email: body.email,
+      });
+      return res.status(200).json(dataUser);
+    } catch (err) {
+      return res.status(400).json(err);
+    }
+  },
   // update user info
-  updateUser({ params, body }, res) {},
+  async updateUser({ params, body }, res) {
+    try {
+      const dataUser = User.findByIdAndUpdate({ _id: params.id }, body, {
+        new: true,
+        runValidators: true,
+      });
+      return dataUser
+        ? res.status(200).json(dataUser)
+        : res.status(404).json({ message: user404Message(params.id) });
+    } catch (err) {
+      return res.status(400).json(err);
+    }
+  },
   // delete user
-  deleteUser({ params }, res) {},
+  async deleteUser({ params }, res) {
+    try {
+      User.findOneAndDelete({ _id: params.id })
+        .then((dbUserData) => {
+          if (!dbUserData) {
+            return res.status(404).json({ message: user404Message(params.id) });
+          }
+          Thought.deleteMany({ username: dbUserData.username }).then(
+            (deletedData) =>
+              deletedData
+                ? res.json({ message: user204Message(params.id) })
+                : res.status(404).json({ message: user404Message(params.id) })
+          );
+        })
+        .catch((err) => res.status(400).json(err));
+    } catch (err) {
+      return res.status(400).json(err);
+    }
+  },
 };
 
 module.exports = userController;
--- models/Thought.js
+++ models/Thought.js
@@ -1,33 +1,5 @@
 const { Schema, model, Types } = require('mongoose')
 
-const ThoughtSchema = new Schema(
-    {
-        thoughtText: {
-            type: String,
-            required: true,
-            minlength: 1,
-            maxLength: 350
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: time
-        },
-        username: {
-            type: String,
-            required: true
-        },
-        reactions: [reactionSchema]
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true 
-        },
-        id: false
-    }
-);
-
 const reactionSchema = new Schema(
     {
         reactionId: {
@@ -46,7 +18,7 @@ const reactionSchema = new Schema(
         createdAt: {
             type: Date,
             default: Date.now,
-            get: time
+            get: () => (Date | number)
         }
     },
     {
@@ -56,6 +28,35 @@ const reactionSchema = new Schema(
         id: false
     }
 );
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxLength: 350
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: () => (Date | number)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true 
+        },
+        id: false
+    }
+);
+
 
 // get total count of reactions
 ThoughtSchema.virtual('reactionCount').get(function() {
