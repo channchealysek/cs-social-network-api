d945aba3ba7cdc132873123b7eaa3b01baf92416
Channchealy Sek <sek.channchealy@yahoo.com>
2022-09-19 11:14:11 -0500

    create structure models User and Thought included reactionSchema

---
+++ models/Thought.js
@@ -0,0 +1,69 @@
+const { Schema, model, Types } = require('mongoose')
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxLength: 350
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: time
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true 
+        },
+        id: false
+    }
+);
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 250
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: time
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+// get total count of reactions
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+// create the Thought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema)
+
+// export the Thought model
+module.exports = Thought
