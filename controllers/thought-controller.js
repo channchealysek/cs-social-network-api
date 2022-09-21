4420edf5651ccd68a902392e3aac4e3b9432a679
Channchealy Sek <sek.channchealy@yahoo.com>
2022-09-20 20:22:21 -0500

    implement routes for thought..GRUD has completed.

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -4,25 +4,31 @@ const thought200Message = (id) => `Thought with ID: ${id} has been deleted!`;
 
 const thoughtController = {
   // get all thoughts
-  getAllThoughts(req, res) {
-    Thought.find({})
-      .populate({ path: "reactions", select: "-__v" })
-      .select("-__v")
-      .then((dataThought) => res.json(dataThought))
-      .catch((err) => res.status(500).json(err));
+  async getAllThoughts(req, res) {
+    try {
+      const dataThought = await Thought.find({})
+        .populate({ path: "reactions", select: "-__v" })
+        .select("-__v");
+      return res.json(dataThought);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
   },
+  
   // get one thought by ID
-  getThoughtById({ params }, res) {
-    Thought.findOne({ _id: params.id })
-      .populate({ path: "reactions", select: "-__v" })
-      .select("-__v")
-      .then((dataThought) =>
-        dataThought
-          ? res.json(dataThought)
-          : res.status(404).json({ message: thought404Message(params.id) })
-      )
-      .catch((err) => res.status(404).json(err));
+  async getThoughtById({ params }, res) {
+    try {
+      const dataThought = await Thought.findOne({ _id: params.id })
+        .populate({ path: "reactions", select: "-__v" })
+        .select("-__v");
+      return dataThought
+        ? res.json(dataThought)
+        : res.status(404).json({ message: thought404Message(params.id) });
+    } catch (err) {
+      return res.status(404).json(err);
+    }
   },
+
   // add a thought
   async createThought({ body }, res) {
     try {
@@ -42,27 +48,34 @@ const thoughtController = {
   },
 
   // update thought info
-  updateThought({ params, body }, res) {
-    Thought.findOneAndUpdate({ _id: params.id }, body, {
-      new: true,
-      runValidators: true,
-    })
-      .then((dataThought) =>
-        dataThought
-          ? res.json(dataThought)
-          : res.status(404).json({ message: thought404Message(params.id) })
-      )
-      .catch((err) => res.status(400).json(err));
+  async updateThought({ params, body }, res) {
+    try {
+      const dataThought = await Thought.findOneAndUpdate(
+        { _id: params.id },
+        body,
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+      return dataThought
+        ? res.json(dataThought)
+        : res.status(404).json({ message: thought404Message(params.id) });
+    } catch (err) {
+      return res.status(400).json(err);
+    }
   },
+
   // delete thought
-  deleteThought({ params }, res) {
-    Thought.findOneAndDelete({ _id: params.id })
-      .then((dataThought) =>
-        dataThought
-          ? res.json(thought200Message(dataThought._id))
-          : res.status(404).json({ message: thought404Message(params.id) })
-      )
-      .catch((err) => res.status(404).json(err));
+  async deleteThought({ params }, res) {
+    try {
+      const dataThought = await Thought.findOneAndDelete({ _id: params.id });
+      return dataThought
+        ? res.json(thought200Message(dataThought._id))
+        : res.status(404).json({ message: thought404Message(params.id) });
+    } catch (err) {
+      return res.status(404).json(err);
+    }
   },
 };
 
